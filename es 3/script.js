f4609ff4eb55bae48346af8a74110ffd3bed654d
Antonioangy <dumitruangy26@gmail.com>
2021-10-28 17:55:35 +0200

    array

--- es 3/script.js
+++ es 3/script.js
@@ -3,21 +3,14 @@
 
 // chiedo all'utente di inserire 9 numeri tramite prompt
 
-const num1 = prompt("inserisci il primo numero");
-const num2 = prompt("inserisci il secondo numero");
-const num3 = prompt("inserisci il terzo numero");
-const num4 = prompt("inserisci il quarto numero");
-const num5 = prompt("inserisci il quinto numero");
-const num6 = prompt("inserisci il sesto numero");
-const num7 = prompt("inserisci il settimo numero");
-const num8 = prompt("inserisci il ottavo numero");
-const num9 = prompt("inserisci il nono numero");
-const num10 = prompt("inserisci il decimo e ultimo numero");
+const risultato = document.getElementById("somma"); 
+
+// creo un array che si riempie di numeri inseriti dall utente
+
+
 
 // sommo numeri e mostro il risultato 
-let totale = (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10);
 
-document.getElementById("somma").innerHTML = totale;
 
 
 
